a675f2a36c220dd986bbc0ffecdfdf9c45e90860
AdityaGupta7 <adityaguptarox@gmail.com>
2019-12-29 16:13:33 +0530

    Updates

---
+++ precache-manifest.54d2dfc265200747d432dfbb8a073d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f69a4689c854b7328180586fcd870f",
+    "url": "/ReactTodoListApp/index.html"
+  },
+  {
+    "revision": "64a31f02d4aa4126c1d1",
+    "url": "/ReactTodoListApp/static/css/main.e2bd4973.chunk.css"
+  },
+  {
+    "revision": "89f98e4af380b06a0276",
+    "url": "/ReactTodoListApp/static/js/2.a0f8c353.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/ReactTodoListApp/static/js/2.a0f8c353.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64a31f02d4aa4126c1d1",
+    "url": "/ReactTodoListApp/static/js/main.b11d4543.chunk.js"
+  },
+  {
+    "revision": "74ae5dc15778c900fc6a",
+    "url": "/ReactTodoListApp/static/js/runtime-main.3211876a.js"
+  }
+]);
